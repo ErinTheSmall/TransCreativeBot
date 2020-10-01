ddf450d0f5d6539773932e1e7f7e71db79b56c57
Erin cooper <36458674+ErinTheSmall@users.noreply.github.com>
2020-10-01 21:18:42 +0100

    remove ctx

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def on_message(message):
             await message.channel.send(embed=embed)
 
     if message.content.startswith('>spoiler'):
-        file = ctx.message.attachments[0]
+        file = message.attachments[0]
         file.filename = f"SPOILER_{file.filename}"
         spoiler = await file.to_file()
         await archiver.send(file=spoiler)
