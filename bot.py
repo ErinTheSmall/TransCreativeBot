2df3d89f072a738cd4069c179de1bf156b67d557
Erin cooper <36458674+ErinTheSmall@users.noreply.github.com>
2020-12-03 19:56:22 +0000

    fix silly syntax error

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def on_member_join(member):
         embed=discord.Embed(title="Welcome to Trans Creative!", color=0xf1c40f)
         embed.set_author(name="Hello,"+member.name, icon_url="https://cdn.discordapp.com/emojis/395628346379206656.png")
         embed.set_thumbnail(url="https://cdn.discordapp.com/icons/696454936942215181/a_d6e6ce8869cbd20f83051542629f94c0.gif")
-        embed.set_footer(text="account created on "+member.joined_at+)
+        embed.set_footer(text="account created on "+member.joined_at)
         channel = client.get_channel(699814426869760119)
         await channel.send(embed=embed)
         
